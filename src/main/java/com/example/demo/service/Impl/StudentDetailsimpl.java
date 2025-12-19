196c430185095d9ffa325b1d48caac4deb4d0933
bala-amypo <bala@amypo.in>
2025-12-19 10:35:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/StudentDetailsimpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service.Impl;
+import org.springframework.stereotype.Service;
+import com.example.demo.service.Studentservice;
+import  com.example.demo.entity.Studententity;
+import  com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class Studentserviceimpl implements Studentservice{
+      @Autowired StudentRepository student;
+      //save()
+      //findall()
+      //findById()
+      //deleteById()
+      //existsById()
+      @Override
+ public Studententity postdata(Studententity stu){
+    return student.save(stu);
+ }
+ @Override
+    public List<Studententity>getAllData(){
+      return student.findAll();
+     }
+@Override
+     public String DeleteData( int id){
+          student.deleteById(id);
+          return "Deleted Sucessfully";
+     }
+@Override
+public Studententity findData(int id){
+    return student.findById(id).orElse(null);
+}
+@Override
+public Studententity UpdateData(int id, Studententity entity){
+      if(student.existsById(id)){
+         entity.setId(id);
+         return student.save(entity);
+      }
+      return null;
+}
+ }
---
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+import com.example.demo.entity.Studententity;
+import java.util.List;
+public interface StudentService{
+    Student postdata(Student stu);
+    
+}
