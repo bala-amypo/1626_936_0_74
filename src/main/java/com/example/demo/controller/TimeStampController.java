960ffe5a53f7796e5b3e437b2f699e887dd8b4fd
bala-amypo <bala@amypo.in>
2025-12-19 08:01:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/TimeStampController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.demo.entity.TimeStamp;
+import com.example.demo.service.TimeStampService;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+
+@RestController
+public class Studentcontroller{
+    @Autowired Studentservice ser;
+@PostMapping("/post")
+public Studententity dopost(@RequestBody Studententity stu){
+    return ser.postdata(stu);
+
+
+}
+@GetMapping("/get")
+public List<Studententity>getvalue(){
+    return ser.getAllData();
+}
+}
--- src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
@@ -1,8 +1,8 @@
 package com.example.demo.service.Impl;
 import org.springframework.stereotype.Service;
-import com.example.demo.service.Studentservice;
-import  com.example.demo.entity.Studententity;
-import  com.example.demo.repository.TimeSRepository;
+import com.example.demo.service.TimeStampService;
+import  com.example.demo.entity.TimeStamp;
+import  com.example.demo.repository.TimeStampRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
